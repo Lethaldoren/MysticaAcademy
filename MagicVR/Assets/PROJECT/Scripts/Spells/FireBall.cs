eafa0ad4acef0a6c3b8be3bbd47ac0020b488a84
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-01-28 11:59:34 -0500

    created fireball prefab
    created fireball prefab for testing and added notes in fireball script

--- MagicVR/Assets/PROJECT/Scripts/Spells/FireBall.cs
+++ MagicVR/Assets/PROJECT/Scripts/Spells/FireBall.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour, EquipableSpell
 {
+    public GameObject fireballPrefab;
+    GameObject fireball;
+
     public FireBall()
     {
 
@@ -12,22 +15,24 @@ public class FireBall : MonoBehaviour, EquipableSpell
     public new void OnEquip()
     {
         Debug.Log("I am Fireball");
+        //create fireball particle effect and have it on hand
     }
 
     public new void OnTriggerDown()
     {
-
+        //create fireball in hand
     }
 
     public new void OnTriggerHeld()
     {
-
+        //keep fireball in hand until trigger released
+        //give croshair for shot direction
     }
 
 
     public new void OnTriggerUp()
     {
-
+        //shoot fireball here
     }
 
 }
