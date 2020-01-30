971156c346311b5f337ed8bc9b245bb1edca968e
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-01-30 16:42:48 -0500

    adjusted wind slash
    added proper namespaces

--- MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
+++ MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
@@ -2,42 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
-namespace Valve.VR.InteractionSystem
+
+public class WindSlash : MonoBehaviour, EquipableSpell
 {
-    public class WindSlash : MonoBehaviour, EquipableSpell
+    //gets hand input and position of hand
+    public SteamVR_Input_Sources Hand;
+    public SteamVR_Action_Pose position;
+
+    private Vector3 velocity;
+
+    public new void OnEquip()
     {
-        //gets hand input and position of hand
-        public SteamVR_Input_Sources Hand;
-        public SteamVR_Action_Pose position;
+        Debug.Log("I am Wind Slash");
+    }
 
-        private Vector3 velocity;
+    public void OnTriggerDown()
+    {
 
-        public new void OnEquip() {
-            Debug.Log("I am Wind Slash");
+    }
+
+    public void OnTriggerHeld()
+    {
+        //gets velocity of hand
+        velocity = position.GetVelocity(Hand);
+
+        //checks if hand is moving
+        if (velocity.x > Vector3.zero.x)
+        {
+            Debug.Log("you're moving the controller");
+            Debug.Log(velocity);
         }
+        //check for velocity and if controller is moving fast
+        //check if it moved 20cm 
+        //get angle and direction of slash (compare head location to slash location)
+        //fire slash in that direction
+    }
 
-        public void OnTriggerDown() {
+    public void OnTriggerUp()
+    {
 
-        }
-
-        public void OnTriggerHeld() {
-            //gets velocity of hand
-            velocity = position.GetVelocity(Hand);
-
-            //checks if hand is moving
-            if (velocity.x > Vector3.zero.x) {
-                Debug.Log("you're moving the controller");
-                Debug.Log(velocity);
-            }
-            //check for velocity and if controller is moving fast
-            //check if it moved 20cm 
-            //get angle and direction of slash (compare head location to slash location)
-            //fire slash in that direction
-        }
-
-        public void OnTriggerUp() {
-
-        }
     }
 }
