87efc1456181704cab1a29fea90352b69df9b227
Beeblesnout <30602106+Beeblesnout@users.noreply.github.com>
2020-01-30 14:53:19 -0500

    Lay out runes

--- MagicVR/Assets/PROJECT/ART/VFX/runeplace.cs
+++ MagicVR/Assets/PROJECT/ART/VFX/runeplace.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 public class runeplace : MonoBehaviour
 {
@@ -10,12 +11,19 @@ public class runeplace : MonoBehaviour
     public float radius;
 
     [ExecuteInEditMode]
-    [MenuItem("Place Runes")]
+    [ContextMenu("Place Runes")]
     public void PlaceRunes()
     {
+        var existingRunes = transform.GetComponentsInChildren<Transform>().ToList();
+        existingRunes.Remove(transform);
+        for (int i = 0; i < existingRunes.Count; i++)
+        {
+            DestroyImmediate(existingRunes[i].gameObject);
+        }
+
         for (int i = 0; i < count; i++)
         {
-            float a = (2 * Mathf.PI) * (i / count);
+            float a = (2 * Mathf.PI) * (i / (float)count);
             Vector3 pos = new Vector3(Mathf.Cos(a), 0, Mathf.Sin(a)) * radius;
             Quaternion rot = Quaternion.Euler(90, 0, a * Mathf.Rad2Deg);
             Instantiate(rune, pos, rot, transform);
