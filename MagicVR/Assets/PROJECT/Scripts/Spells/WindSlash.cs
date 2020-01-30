279c9dc9968a522d13b25c6cc16a4715b1972757
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-01-30 10:46:42 -0500

    added wisn slash script
    started wind slash script and added what is needed

---
+++ MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindSlash : MonoBehaviour, EquipableSpell
+{
+    public new void onEquip()
+    {
+        Debug.Log("I am Wind Slash");
+    }
+
+    public void onTriggerDown()
+    {
+        //track position on controller position
+    }
+
+    public void OnTriggerHeld()
+    {
+        //check for velocity and if controller is moving fast
+        //check if it moved 20cm 
+        //get angle and direction of slash (compare head location to slash location)
+        //fire slash in that direction
+    }
+
+    public void OnTriggerUp()
+    {
+
+    }
+}
