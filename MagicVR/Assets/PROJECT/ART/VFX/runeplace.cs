4ddb554f0ce1cd5ef1d0e202c2d90cdd0be25d99
Beeblesnout <30602106+Beeblesnout@users.noreply.github.com>
2020-01-30 13:22:51 -0500

    Upload particle textures

---
+++ MagicVR/Assets/PROJECT/ART/VFX/runeplace.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class runeplace : MonoBehaviour
+{
+    public GameObject rune;
+    public int count;
+    public float radius;
+
+    [ExecuteInEditMode]
+    [MenuItem("Place Runes")]
+    public void PlaceRunes()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            float a = (2 * Mathf.PI) * (i / count);
+            Vector3 pos = new Vector3(Mathf.Cos(a), 0, Mathf.Sin(a)) * radius;
+            Quaternion rot = Quaternion.Euler(90, 0, a * Mathf.Rad2Deg);
+            Instantiate(rune, pos, rot, transform);
+        }
+    }
+}
