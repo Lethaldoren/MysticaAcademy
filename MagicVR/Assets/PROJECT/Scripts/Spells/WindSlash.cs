cebea6262a03360c4f3dc120f86d1b3c845acd5f
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-01-30 17:09:59 -0500

    windslash progress
    added checks for magnitude and time at speed to spawn slash

--- MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
+++ MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
@@ -14,9 +14,12 @@ public class WindSlash : MonoBehaviour, EquipableSpell
 
     private Vector3 velocity;
 
+    float swingTimer;
+
     public new void OnEquip()
     {
         Debug.Log("I am Wind Slash");
+        swingTimer = 0;
     }
 
     public void OnTriggerDown()
@@ -29,20 +32,51 @@ public class WindSlash : MonoBehaviour, EquipableSpell
         //gets velocity of hand
         velocity = position.GetVelocity(Hand);
 
-        //checks if hand is moving
+        //checks if hand is moving ====remove after testing====
         if (velocity.x > Vector3.zero.x)
         {
             Debug.Log("you're moving the controller");
             Debug.Log(velocity);
         }
-        //check for velocity and if controller is moving fast
-        //check if it moved 20cm 
-        //get angle and direction of slash (compare head location to slash location)
-        //fire slash in that direction
+
+        float swingSpeed = velocity.magnitude;
+
+        if (swingSpeed > 5)
+        {
+            Debug.Log("swinging");
+            swingTimer += Time.deltaTime;
+
+            //gets position of hand when it moves fast enough
+            Vector3 startPos = position.localPosition;
+
+            if (swingTimer >= 0.4f)
+            {
+                if (swingTimer < 5)
+                {
+                    Vector3 endPos = position.localPosition;
+                    swingTimer = 0;
+                    spawnSlash();
+                }
+            }
+        }
+        if (swingSpeed > 5)
+        {
+            swingTimer = 0;
+        }
     }
 
     public void OnTriggerUp()
     {
 
+    }
+
+    private void spawnSlash()
+    {
+
+        /* spawn slash at point between start and end position
+         * adjust angle based on those values
+         * add velocity to move away from head position
+         * destroy after X seconds
+         */
     }
 }
