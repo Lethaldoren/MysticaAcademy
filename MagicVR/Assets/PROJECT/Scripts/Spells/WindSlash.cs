e028e4a603aca0997913065c262f36542b4e8664
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-01-30 11:21:40 -0500

    getting controller velocity
    changed wind slash to get velocity of controllers on trigger hold

--- MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
+++ MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
@@ -2,28 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindSlash : MonoBehaviour, EquipableSpell
+
+namespace Valve.VR.InteractionSystem
 {
-    public new void onEquip()
+    public class WindSlash : MonoBehaviour, EquipableSpell
     {
-        Debug.Log("I am Wind Slash");
-    }
+        SteamVR_TrackedObject trackedObject;
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedObj.index);
 
-    public void onTriggerDown()
-    {
-        //track position on controller position
-    }
+        public new void onEquip()
+        {
+            Debug.Log("I am Wind Slash");
+        }
 
-    public void OnTriggerHeld()
-    {
-        //check for velocity and if controller is moving fast
-        //check if it moved 20cm 
-        //get angle and direction of slash (compare head location to slash location)
-        //fire slash in that direction
-    }
+        public void onTriggerDown()
+        {
+            //track position of controller
+        }
 
-    public void OnTriggerUp()
-    {
+        public void OnTriggerHeld()
+        {
+            Vector3 vel = device.velocity;
+            if (velocity > 0)
+            {
+                debug.log("you're moving the controller");
+                debug.log(velocity);
+            }
+            //check for velocity and if controller is moving fast
+            //check if it moved 20cm 
+            //get angle and direction of slash (compare head location to slash location)
+            //fire slash in that direction
+        }
 
+        public void OnTriggerUp()
+        {
+
+        }
     }
 }
