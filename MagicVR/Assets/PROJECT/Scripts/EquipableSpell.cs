12be2883cee277a0e313030cceae7a4080d24038
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-01-30 21:54:00 -0500

    Revert "Merge branch 'Spell-Equip-Changes' into Conor-Spells"
    This reverts commit e0633fb1f19b1c9dc7f0e3aa7371d0d7f45168ae, reversing
    changes made to cebea6262a03360c4f3dc120f86d1b3c845acd5f.

--- MagicVR/Assets/PROJECT/Scripts/EquipableSpell.cs
+++ MagicVR/Assets/PROJECT/Scripts/EquipableSpell.cs
@@ -1,26 +1,16 @@
 ﻿
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-[Serializable]
-public class EquipableSpell : MonoBehaviour
+public interface EquipableSpell
 {
-    public string magicWords;
-    public GameObject spellPrefab;
-
     // what occurs when the spell is originally equiped
-    public virtual void OnEquip()
-    {
-        print("fuck you");
-    }
-    public virtual void OnTriggerDown() { }
-    public virtual void OnTriggerHeld() { }
-    public virtual void OnTriggerUp() { }
-    public virtual void OnUnequip()
-    {
-        Destroy(gameObject);
-    }
+    void OnEquip();
+
+    void OnTriggerDown();
+
+    void OnTriggerHeld();
+
+    void OnTriggerUp();
 }
--- MagicVR/Assets/PROJECT/Scripts/Spells/FireBall.cs
+++ MagicVR/Assets/PROJECT/Scripts/Spells/FireBall.cs
@@ -2,35 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MagmaBallSpell : EquipableSpell
+public class FireBall : MonoBehaviour, EquipableSpell
 {
+    public GameObject fireballPrefab;
     public Transform wandTip;
     GameObject fireball;
     Rigidbody frb;
 
     float speed = 4000;
 
-    public override void OnEquip()
+
+    public new void OnEquip()
     {
-        Debug.Log("I am Magmaball");
+        Debug.Log("I am Fireball");
         //create fireball particle effect and have it on hand
     }
 
-    public override void OnTriggerDown()
+    public void OnTriggerDown()
     {
         //create fireball and change parent
-        fireball = Instantiate(spellPrefab, wandTip);
+        fireball = Instantiate(fireballPrefab, wandTip);
         frb = fireball.GetComponent<Rigidbody>();
     }
 
-    public override void OnTriggerHeld()
+    public void OnTriggerHeld()
     {
         //keep fireball in hand until trigger released
         //give croshair for shot direction
     }
 
 
-    public override void OnTriggerUp()
+    public void OnTriggerUp()
     {
         //records position at release and unparents from hand and moves to position at release
         Transform currentPosition = fireball.transform;
@@ -38,16 +40,10 @@ public class MagmaBallSpell : EquipableSpell
         fireball.transform.position = currentPosition.transform.position;
 
         //shoots fireball forward at speed
-        if (frb != null)
-        {
+        if (frb != null) {
             frb.AddRelativeForce(Vector3.forward * speed);
         }
         //destroy prefab after 5 seconds or contact
-    }
-
-    public override void OnUnequip()
-    {
-
     }
 
 }
