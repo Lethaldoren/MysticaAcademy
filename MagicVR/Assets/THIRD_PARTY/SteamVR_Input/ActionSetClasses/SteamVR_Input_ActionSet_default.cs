30f4a7c7fe96cf3effd0146371d196d5512ed62c
Lethaldoren <48733718+Lethaldoren@users.noreply.github.com>
2020-01-31 17:00:05 -0500

    Progress on teleport controlls
    -Adjusted Input Actions
    -Added teleport test scene
    - Updated materials on some Steam VR Assests

--- MagicVR/Assets/THIRD_PARTY/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ MagicVR/Assets/THIRD_PARTY/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Vector2 TeleportTurn
+        {
+            get
+            {
+                return SteamVR_Actions.default_TeleportTurn;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
