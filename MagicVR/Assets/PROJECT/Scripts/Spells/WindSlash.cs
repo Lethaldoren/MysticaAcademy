5248c14c4bf02298b54990b4f1fd5561c0cb4be6
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-01-30 21:54:02 -0500

    Revert "Merge branch 'Conor-Spells' of https://github.com/Lethaldoren/MagicVR into Conor-Spells"
    This reverts commit f0120d3838c64d54ae4dd6a7a1241ee1dc440569, reversing
    changes made to e0633fb1f19b1c9dc7f0e3aa7371d0d7f45168ae.

--- MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
+++ MagicVR/Assets/PROJECT/Scripts/Spells/WindSlash.cs
@@ -10,17 +10,9 @@ public class WindSlash : MonoBehaviour, EquipableSpell
 {
     //gets hand input and position of hand
     public SteamVR_Input_Sources Hand;
-    public SteamVR_Action_Pose handPosition;
-    public SteamVR_Input_Sources Head;
-    public SteamVR_Action_Pose headPosition;
-
-    public GameObject slashPrefab;
-    Rigidbody srb;
-    public float speed;
+    public SteamVR_Action_Pose position;
 
     private Vector3 velocity;
-    private Vector3 startPos;
-    private Vector3 endPos;
 
     float swingTimer;
 
@@ -38,8 +30,7 @@ public class WindSlash : MonoBehaviour, EquipableSpell
     public void OnTriggerHeld()
     {
         //gets velocity of hand
-        velocity = handPosition.GetVelocity(Hand);
-        srb = slashPrefab.GetComponent<Rigidbody>();
+        velocity = position.GetVelocity(Hand);
 
         //checks if hand is moving ====remove after testing====
         if (velocity.x > Vector3.zero.x)
@@ -56,14 +47,13 @@ public class WindSlash : MonoBehaviour, EquipableSpell
             swingTimer += Time.deltaTime;
 
             //gets position of hand when it moves fast enough
-            startPos = handPosition.localPosition;
+            Vector3 startPos = position.localPosition;
 
             if (swingTimer >= 0.4f)
             {
-                //chacks if hand slows down to finish the slash
                 if (swingTimer < 5)
                 {
-                    endPos = handPosition.localPosition;
+                    Vector3 endPos = position.localPosition;
                     swingTimer = 0;
                     spawnSlash();
                 }
@@ -80,29 +70,12 @@ public class WindSlash : MonoBehaviour, EquipableSpell
 
     }
 
-    //spawns slash prefab in location, rotation, and scale that was drawn then makes it move
     private void spawnSlash()
     {
-        Vector3 midPoint = (startPos + endPos) / 2;
-
-        //spawns object in space and adjusts rotation based on points
-        GameObject slash = Instantiate(slashPrefab, midPoint, Quaternion.FromToRotation(Vector3.up, endPos - startPos));
-
-        //change scale of prefab based on 2 points
-        float scaleX = Mathf.Abs(startPos.x - endPos.x);
-        float scaleY = Mathf.Abs(startPos.y - endPos.y);
-        float scaleZ = Mathf.Abs(startPos.z - endPos.z);
-
-        slash.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
-
-        //add velocity based on headset position 
-        Vector3 positionOfHead = headPosition.localPosition;
-        srb.AddRelativeForce((positionOfHead - slash.transform.position).normalized * speed); //swap transformes if it moves towards
 
         /* spawn slash at point between start and end position
          * adjust angle based on those values
-         * scale based on points
-         * add velocity to move away from head position (change to hand position average through swipe)
+         * add velocity to move away from head position
          * destroy after X seconds
          */
     }
