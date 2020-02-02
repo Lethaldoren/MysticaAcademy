9187a28497bf1265f7821b71821d58237be47fe7
Adam Gill <30602106+Beeblesnout@users.noreply.github.com>
2020-02-02 05:00:10 -0500

    Finalize rune ring VFX

--- MagicVR/Assets/PROJECT/ART/VFX/Scripts/RuneManager.cs
+++ MagicVR/Assets/PROJECT/ART/VFX/Scripts/RuneManager.cs
@@ -4,67 +4,33 @@ using UnityEngine;
 
 public class RuneManager : MonoBehaviour
 {
-    public GameObject rune;
+    public GameObject runeTemplate;
     public int count;
     public float radius;
-    
-    public List<Rune> runes = new List<Rune>();
+
+    public Sprite[] runes;
 
     [ExecuteInEditMode]
-    [ContextMenu("Place Runes")]
-    public void PlaceRunes()
+    [ContextMenu("Spawn Runes")]
+    public void SpawnRunes()
     {
-        runes.ForEach(r => DestroyImmediate(r.gameObject));
-        runes.Clear();
-
         for (int i = 0; i < count; i++)
         {
             float a = (2 * Mathf.PI) * (i / (float)count);
             Vector3 pos = new Vector3(Mathf.Cos(a), 0, Mathf.Sin(a)) * radius;
             Quaternion rot = Quaternion.Euler(90, 0, a * Mathf.Rad2Deg - 90);
-            runes.Add(new Rune(Instantiate(rune, pos, rot, transform), count));
+            Instantiate(runeTemplate, pos, rot, transform);
         }
     }
     
     [ExecuteInEditMode]
     [ContextMenu("Randomize Rune Index Offsets")]
-    public void RandomizeOffsets()
+    public void RandomizeTextures()
     {
-        for (int i = 0; i < runes.Count; i++)
+        var r = transform.GetComponentsInChildren<SpriteRenderer>();
+        for (int i = 0; i < r.Length; i++)
         {
-            runes[i].RuneIndex = (int)Random.Range(0, count);
+            r[i].sprite = runes[Random.Range(0, runes.Length)];
         }
-    }
-}
-
-public class Rune
-{
-    public GameObject gameObject;
-    // public Material mat;
-    public float maxValue;
-    private float p_runeIndex;
-    [ContextMenuItem("Randomize Offset", "RandomizeOffset")]
-    public float indexOffset;
-    public float RuneIndex
-    {
-        get { return p_runeIndex; }
-        set
-        {
-            p_runeIndex = Mathf.Repeat(value + indexOffset, maxValue);
-        }
-    }
-
-    public Rune(GameObject runeObj, float maxValue)
-    {
-        gameObject = runeObj;
-        // mat = runeObj.GetComponent<MeshRenderer>().material;
-        this.maxValue = maxValue;
-        indexOffset = 0;
-        RuneIndex = (int)Random.Range(0, maxValue);
-    }
-
-    public void Randomize()
-    {
-        RuneIndex = (int)Random.Range(0, maxValue);
     }
 }
