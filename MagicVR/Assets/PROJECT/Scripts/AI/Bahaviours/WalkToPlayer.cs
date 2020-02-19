7b121912eb3e39d2e49217e444aff080f70f0833
Lethaldoren <48733718+Lethaldoren@users.noreply.github.com>
2020-02-19 12:46:58 -0500

    Imported Behavoir Machine

---
+++ MagicVR/Assets/PROJECT/Scripts/AI/Bahaviours/WalkToPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BehaviourMachine;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkToPlayer : StateBehaviour 
+{
+    public GameObjectVar player;
+    Vector3 playerPosition;
+    private NavMeshAgent agent;
+
+    private FloatVar UpdateDelay;
+    WaitForSeconds wait;
+    
+    private void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine("UpdateTargetPosition");
+
+        wait = new WaitForSeconds(UpdateDelay.Value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    public IEnumerator UpdateTargetPosition()
+    {
+        while(true)
+        {
+            GetPlayerPosition();
+            agent.SetDestination(playerPosition);
+            yield return wait;
+        }
+    }
+
+    void GetPlayerPosition()
+    {
+        
+    }
+}
