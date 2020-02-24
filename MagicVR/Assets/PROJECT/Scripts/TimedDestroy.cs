b1b4c37b0f16fa651f15f7bac5aa2cbc08eee9a9
Adam Gill <pocketman3915@gmail.com>
2020-02-24 04:39:09 -0500

    Create dummy spawner

---
+++ MagicVR/Assets/PROJECT/Scripts/TimedDestroy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour
+{
+    public static void DestroyAfterTime()
+    {
+        
+    }
+
+    public float delay;
+    float timeRemaining;
+
+    void Start()
+    {
+        timeRemaining = delay;
+    }
+
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
