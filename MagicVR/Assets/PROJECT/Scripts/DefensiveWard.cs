582e367a04ab5cc0ffc88dda0255ab3ebd8ecf28
Lethaldoren <48733718+Lethaldoren@users.noreply.github.com>
2020-03-11 14:52:29 -0400

    DefensiveSpell

---
+++ MagicVR/Assets/PROJECT/Scripts/DefensiveWard.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Valve.VR.InteractionSystem
+{
+    public class DefensiveWard : MonoBehaviour
+    {
+
+        public float objectMoveSpeed;
+        public float targetGrabRange;
+
+        public float forceMultplier;
+
+        bool holdingObject;
+        Rigidbody heldObject;
+
+        Hand hand;
+
+    void SearchForObject ()
+        {
+
+        }
+
+    void OnObjectPickUp(Rigidbody newObject)
+        {
+            holdingObject = true;
+            heldObject = newObject;
+
+        }
+
+
+        void DragObject()
+        {
+           
+            Vector3 TargetPosition = hand.transform.right;
+        }
+        void ThrowObject()
+        {
+
+        }
+
+        void StopAttack()
+        {
+
+        }
+
+        void DragEnemy()
+        {
+
+        }
+
+        void ThrowEnemy()
+        {
+
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
