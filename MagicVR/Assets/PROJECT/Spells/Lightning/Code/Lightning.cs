8bb1a621f500a0d5653fd24f0895ea1fd6566fd7
Adam Gill <pocketman3915@gmail.com>
2020-03-30 20:03:23 -0400

    Create rough lightning VFX

---
+++ MagicVR/Assets/PROJECT/Spells/Lightning/Code/Lightning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour
+{
+    public float charge;
+    public float chargeTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
