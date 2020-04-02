a26a641452fe4f038e90230a50eaa0361ad20d9e
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-04-02 16:14:03 -0400

    finished AI state Machines
    finished AI attack states so they now damage the player all states for both melee and ranged enemy are finished

--- MagicVR/Assets/PROJECT/Scripts/AI/Melee Goblin/Attack.cs
+++ MagicVR/Assets/PROJECT/Scripts/AI/Melee Goblin/Attack.cs
@@ -5,8 +5,22 @@ using BehaviourMachine;
 
 public class Attack : StateBehaviour
 {
-	// Called when the state is enabled
-	void OnEnable () {
+    GameObjectVar playerObject;
+    PlayerHealth playerHealthScr;
+
+    public float hitDamage;
+
+    private void Awake() {
+        playerObject = blackboard.GetGameObjectVar("PlayerObject");
+
+        playerHealthScr = playerObject.Value.GetComponent<PlayerHealth>();
+    }
+
+    // Called when the state is enabled
+    void OnEnable () {
+
+        playerHealthScr.Damage(hitDamage);
+
         SendEvent("Attacked");
 	}
 
