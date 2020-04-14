d194080bf4ee3e75f7546688117afffb945cadfd
Lethaldoren <48733718+Lethaldoren@users.noreply.github.com>
2020-04-14 16:57:25 -0400

    Started Work On Stealth Prototype
    +added new teleportation mechanic
    + added new telekinis mechanic
    + update unity version
    + imported polygon viking pack
    + created stealth prototype scene

---
+++ MagicVR/Assets/PROJECT/Scripts/Player/MovingTeleport.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+
+
+    public class MovingTeleport : MonoBehaviour
+    {
+        public Player player;
+        public LayerMask CollisionMask;
+        public GameObject TeleportMarker;
+
+        public SteamVR_Action_Boolean teleportAction;
+        public SteamVR_Action_Boolean acceptTeleportAction; 
+
+        Hand pointerHand; 
+
+
+        public float MarkerMoveSpeed;
+        public float FadeTime; 
+        Vector3 TargetPosition;
+        Vector3 MarkerPosition;
+
+        bool markerActive;
+        bool teleporting; 
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //player = InteractionSystem.Player.instance;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckInput();
+
+            if (markerActive)
+                MoveMarker(); 
+
+        }
+
+        void CheckInput()
+        {
+            foreach (Hand hand in player.hands)
+            {
+                if (teleportAction.GetStateDown(hand.handType) && !markerActive)
+                {
+                    pointerHand = hand;
+                    ActivateTeleportMarker();
+                }
+
+                if (markerActive && hand == pointerHand)
+                {
+                    if (acceptTeleportAction.GetStateDown(hand.handType))
+                    {
+                        BeginPlayerTeleport();
+                    }
+
+                    if (teleportAction.GetStateUp(hand.handType))
+                    {
+                        DeactivateTeleportMarker(); 
+                    }                    
+                }
+            }
+        }
+
+        void BeginPlayerTeleport()
+        {
+            teleporting = false;
+            SteamVR_Fade.Start(Color.clear, 0);
+            SteamVR_Fade.Start(Color.black, FadeTime);
+
+            Invoke("TeleportPlayer", FadeTime);
+        }
+
+        void TeleportPlayer ()
+        {
+            teleporting = false;
+
+            Vector3 playerFeetOffset = player.trackingOriginTransform.position - player.feetPositionGuess;
+            player.trackingOriginTransform.position = TeleportMarker.transform.position + playerFeetOffset;
+
+            DeactivateTeleportMarker(); 
+        }
+
+        void ActivateTeleportMarker ()
+        {
+            markerActive = true;
+            TeleportMarker.SetActive(true);
+        }
+
+        void DeactivateTeleportMarker()
+        {
+            markerActive = false;
+            TeleportMarker.SetActive(false);
+        }
+
+        void GetTargetPosition()
+        {
+            Transform handTransform = pointerHand.transform;
+            RaycastHit hit;
+
+            Ray ray = new Ray(handTransform.position, handTransform.forward);
+
+            if (Physics.Raycast (ray, out hit,Mathf.Infinity, CollisionMask))
+            {
+                Debug.DrawRay(handTransform.position, handTransform.forward, Color.red);
+                TargetPosition = hit.point; 
+            }
+        }
+
+        void MoveMarker()
+        {
+            GetTargetPosition();
+            Vector3 TargetDirection = (TargetPosition - TeleportMarker.transform.position).normalized;
+            TeleportMarker.transform.Translate(TargetDirection * (MarkerMoveSpeed * Time.deltaTime)); 
+        }
+    }
---
+++ MagicVR/Assets/PROJECT/Scripts/Player/Telekinisis.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem; 
+
+public class Telekinisis : MonoBehaviour
+{
+    Player player; 
+    Hand castingHand;
+    public SteamVR_Action_Boolean castAction;
+
+    public Vector3 castExtents;
+    public float maxCastDistance;
+    public float castRadius;
+
+    public LayerMask collisionMask;
+
+    public float MinVelocity;
+    public float VelocityToThrow; 
+
+    bool searchingForObject;
+    bool controllingObject;
+    Rigidbody controlledRB;
+    Vector3 controllerOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput(); 
+
+        if (searchingForObject)
+        SearchForObject();
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (controllingObject)
+        {
+            GetControllerGesture();
+        }
+    }
+
+    void GetControllerGesture ()
+    {
+
+        Vector3 velocity;
+        Vector3 angularVelocity;
+
+        castingHand.GetEstimatedPeakVelocities(out velocity, out angularVelocity);
+
+
+        Vector3 TargetDirection =  castingHand.transform.position - controllerOrigin; 
+
+        if (velocity.magnitude > VelocityToThrow)
+        {
+            ThrowObject(TargetDirection, velocity.magnitude);
+        }
+        else if (velocity.magnitude > MinVelocity)
+        {
+            MoveObject(TargetDirection);
+        } else
+        {
+            controlledRB.velocity = Vector3.zero;
+        }
+    }
+
+    void MoveObject(Vector3 direction)
+    {
+       
+        controlledRB.velocity = direction;
+        
+    }
+
+    void ThrowObject(Vector3 direction, float speed)
+    {
+        Vector3 velocity;
+        Vector3 angularVelocity;
+
+        castingHand.GetEstimatedPeakVelocities(out velocity, out angularVelocity);
+        controlledRB.velocity = (velocity.normalized * velocity.sqrMagnitude * .5f);
+
+        ReleaseObject();
+    }
+
+    void CheckInput()
+    {
+        foreach(Hand hand in player.hands)
+        {
+            if (!controllingObject && !searchingForObject && castAction.GetStateDown(hand.handType))
+            {
+                castingHand = hand;
+                searchingForObject = true;
+            }
+
+            if (controllingObject && castAction.GetStateUp(hand.handType))
+                ReleaseObject();
+
+            if (searchingForObject && castAction.GetStateUp(hand.handType))
+                searchingForObject = false;
+        }
+    }
+
+    
+
+    void SearchForObject()
+    {
+        Debug.Log("Searching For Object");
+        Transform handTransform = castingHand.gameObject.transform;
+
+        RaycastHit[] hits = Physics.CapsuleCastAll(handTransform.position, handTransform.position + (handTransform.forward * maxCastDistance), castRadius, handTransform.forward, Mathf.Infinity, collisionMask);
+        if (hits.Length > 0)
+        {
+            Debug.Log(hits[0].collider.gameObject);
+            controlledRB = hits[0].collider.attachedRigidbody;
+            GrabObject();
+        }
+    }
+
+    void GrabObject ()
+    {
+        searchingForObject = false;
+        controllingObject = true;
+
+        controlledRB.useGravity = false;
+        controllerOrigin = castingHand.transform.position;
+    }
+
+
+    void ReleaseObject ()
+    {
+        controllingObject = false;
+        controlledRB.useGravity = true; 
+    }
+
+}
