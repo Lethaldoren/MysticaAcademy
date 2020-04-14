f9137b1867ec998a1866f0886dcac0115afc18d0
Conor-Norman <45054425+Conor-Norman@users.noreply.github.com>
2020-04-14 14:27:03 -0400

    added new AI and imported vision cone script
    created new scout ai and imported visioc cone script and prefab

---
+++ MagicVR/Assets/PROJECT/Scripts/AI/Scout/RoamAround.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using BehaviourMachine;
+using UnityEngine.AI;
+
+public class RoamAround : StateBehaviour
+{
+    public NavMeshAgent agent;
+    public float roamSpeed;
+
+
+	// Called when the state is enabled
+	void OnEnable () {
+        agent.speed = roamSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
+
+
