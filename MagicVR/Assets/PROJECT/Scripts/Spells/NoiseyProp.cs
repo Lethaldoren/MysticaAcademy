fbb2ddef8b261ed713b1d06e9251847042eb3359
Lethaldoren <48733718+Lethaldoren@users.noreply.github.com>
2020-04-15 13:56:58 -0400

    Scene Updates

---
+++ MagicVR/Assets/PROJECT/Scripts/Spells/NoiseyProp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourMachine;
+
+public class NoiseyProp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float ForceToAlert;
+    public float NoiseRadius;
+    public LayerMask enemyLayerMask;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+       if (collision.impulse.magnitude > ForceToAlert)
+        {
+            Collider[] colliders = Physics.OverlapSphere(rb.transform.position, NoiseRadius, enemyLayerMask);
+
+            foreach (Collider collider in colliders)
+            {
+                Blackboard blackboard = collider.gameObject.GetComponent<Blackboard>();
+
+                // insert the name of the event connor had setup. 
+                blackboard.SendEvent("");
+            }
+        }
+    }
+}
